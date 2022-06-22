f58a360492442767845f88e3597aa54ba62eb697
ThinhNguyen <phuocthinhdn12023@gmail.com>
2022-06-22 20:12:44 +0700

    commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,8 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import LoginForm from './LoginForm';
-import { useNavigate } from 'react-router';
 import "./LoginForm.css";
 
 const Login = (props) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    let navigate = useNavigate();
-
-    const submitHandler = (event) => {
-        event.preventDefault();
-        // console.log(username, password);
-        const baseUrl = process.env.REACT_APP_ROOT_API;
-        axios.post(`${baseUrl}/user/login`, {
-            username: username,
-            password: password,
-        }).then(res => {
-            console.log(res);
-            localStorage.setItem('token', res.data.token);
-            navigate('/main');
-        }).catch(err => {
-            console.log(err);
-        })
-    }
     return (
         <>
             <LoginForm></LoginForm>
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,31 @@
 import background from "../img/formbg2.jpg";
 import google from "../img/google.png"
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router';
 
 
-const LoginForm = () => {
+const LoginForm = (props) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    let navigate = useNavigate();
+
+    const submitHandler = (event) => {
+        // event.preventDefault();
+        // console.log(username, password);
+        const baseUrl = process.env.REACT_APP_ROOT_API;
+        axios.post(`${baseUrl}/user/login`, {
+            username: username,
+            password: password,
+        }).then(res => {
+            console.log(res);
+            localStorage.setItem('token', res.data.token);
+            navigate('/main');
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+
     return (
         <div className="login__container">
         <img src={background} alt=""/>
@@ -12,10 +35,10 @@ const LoginForm = () => {
                     <h2>Welcome</h2>
                 </div>
                 <div className="login__account">
-                    <input type="text" placeholder="Enter your email"/>
+                    <input type="text" placeholder="Enter your email" value={username} onChange={event => setUsername(event.target.value)} />
                 </div>
                 <div className="login__password">
-                    <input type="password" placeholder="Enter your password"/>
+                    <input type="password" placeholder="Enter your password" onChange={event => setPassword(event.target.value)}/>
                 </div>
                 <div className="login__utilities">
                     <div className="login__rememberme">
@@ -28,7 +51,7 @@ const LoginForm = () => {
                 </div>
                 <div className="login__signin">
                     <div className="login__signin--btn">
-                        <span>Sign in</span>
+                        <span onClick={submitHandler}>Sign in</span>
                     </div>
                 </div>
                 <div className="login__google">
