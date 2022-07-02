4748dfdaceb173838b59171b70c66d192930e031
quanglamm <quanglam180903@gmail.com>
2022-07-02 19:08:13 +0700

    update

--- src/components/HabitForm.js
+++ src/components/HabitForm.js
@@ -71,9 +71,49 @@ const HabitForm = (props) => {
                 <input type="checkbox"/>
                 <label className="queryform__style--1">Send notification automatically</label>
             </div>
-            <div className="form__query--1 mt-32">
+            {/* <div className="form__query--1 mt-32">
                 <label>Importance rate of habit</label>
                 <input type="text" placeholder="City, airport, region, district..." className="queryform__style--1" onChange={importanceChangeHandler} value={habitImportance}/>
+            </div> */}
+            <div className="form__radio mt-32">
+                <span>Importance rate of habit</span>
+                
+                <div className="radio__container">
+                    <label className="radio__label">
+                        <input type="radio" name="radio"/>
+                        <span className="radio__design"></span>
+                        <span className="radio__text">Low</span>
+                    </label>
+                    
+                    <label className="radio__label">
+                        <input type="radio" name="radio"/>
+                        <span className="radio__design"></span>
+                        <span className="radio__text">Moderate</span>
+                    </label>
+                    
+                    <label className="radio__label">
+                        <input type="radio" name="radio"/>
+                        <span className="radio__design"></span>
+                        <span className="radio__text">High</span>
+                    </label>
+                </div>
+            </div>
+            <div className="form__radio mt-32">
+                <span>Habit type</span>
+                
+                <div className="radio__container">
+                    <label className="radio__label">
+                        <input type="radio" name="radio"/>
+                        <span className="radio__design"></span>
+                        <span className="radio__text">Create a good habit</span>
+                    </label>
+                    
+                    <label className="radio__label">
+                        <input type="radio" name="radio"/>
+                        <span className="radio__design"></span>
+                        <span className="radio__text">Break a bad habit</span>
+                    </label>
+                </div>
             </div>
             <div className="form__apply">
                 <button className='form__apply--btn' type='submit'>
