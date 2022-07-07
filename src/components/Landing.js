16a41c2f1a6e96c869486b8bba072d7bfcb07f6e
ThinhNguyen <phuocthinhdn12023@gmail.com>
2022-07-07 08:40:11 +0700

    commit

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = (props) => {
     return (
         <>
             <h1>This is the landing page</h1>
+            <Link to="/login">Sign in</Link>
+            <Link to="/register">Sign Up</Link>
         </>
     )
 }
